d40a8210330f3c7f42151b2d93e845eccbba47c4
seokhoho <kreel9402@gmail.com>
2022-10-26 05:52:56 +0900

    Initial Commit

---
+++ Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notice
+//
+//  Created by 정유진 on 2022/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
